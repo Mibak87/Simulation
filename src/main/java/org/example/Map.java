ee5f21b5986b1f02bbc3869204efdb761ee13c76
Mikhail Bakulin <mibak@yandex.ru>
2023-11-06 21:26:39 +0300

    Add creation of entity in method startSimulation. Change constructor of class Map.

--- src/main/java/org/example/Map.java
+++ src/main/java/org/example/Map.java
@@ -7,58 +7,64 @@ import org.example.staticentity.Rock;
 import org.example.staticentity.Tree;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Map {
-    private HashSet<Herbivore> herbivores;
-    private HashSet<Predator> predators;
-    private HashSet<Grass> grasses;
-    private HashSet<Rock> rocks;
-    private HashSet<Tree> trees;
+    private Set<Herbivore> herbivores;
+    private Set<Predator> predators;
+    private Set<Grass> grasses;
+    private Set<Rock> rocks;
+    private Set<Tree> trees;
     private int width;
     private int height;
 
     public Map(int width, int height) {
         this.width = width;
         this.height = height;
+        Set<Herbivore> herbivores = new HashSet<>();
+        Set<Predator> predators = new HashSet<>();
+        Set<Grass> grasses = new HashSet<>();
+        Set<Rock> rocks = new HashSet<>();
+        Set<Tree> trees = new HashSet<>();
     }
 
-    public HashSet<Herbivore> getHerbivores() {
+    public Set<Herbivore> getHerbivores() {
         return herbivores;
     }
 
-    public HashSet<Predator> getPredators() {
+    public Set<Predator> getPredators() {
         return predators;
     }
 
-    public HashSet<Grass> getGrasses() {
+    public Set<Grass> getGrasses() {
         return grasses;
     }
 
-    public HashSet<Rock> getRocks() {
+    public Set<Rock> getRocks() {
         return rocks;
     }
 
-    public HashSet<Tree> getTrees() {
+    public Set<Tree> getTrees() {
         return trees;
     }
 
-    public void setHerbivores(HashSet<Herbivore> herbivores) {
+    public void setHerbivores(Set<Herbivore> herbivores) {
         this.herbivores = herbivores;
     }
 
-    public void setPredators(HashSet<Predator> predators) {
+    public void setPredators(Set<Predator> predators) {
         this.predators = predators;
     }
 
-    public void setGrasses(HashSet<Grass> grasses) {
+    public void setGrasses(Set<Grass> grasses) {
         this.grasses = grasses;
     }
 
-    public void setRocks(HashSet<Rock> rocks) {
+    public void setRocks(Set<Rock> rocks) {
         this.rocks = rocks;
     }
 
-    public void setTrees(HashSet<Tree> trees) {
+    public void setTrees(Set<Tree> trees) {
         this.trees = trees;
     }
 
--- src/main/java/org/example/Simulation.java
+++ src/main/java/org/example/Simulation.java
@@ -1,19 +1,66 @@
 package org.example;
 
 
+import org.example.creatures.Herbivore;
+import org.example.creatures.Predator;
+import org.example.staticentity.Grass;
+import org.example.staticentity.Rock;
+import org.example.staticentity.Tree;
+
+import java.util.Set;
+
 public class Simulation {
     private Map map;
-    private int moveCounter;
+    private int moveCounter = 0;
     private Render render;
     private Actions actions;
+    private int countHerbivore = 1;
+    private int countPredator = 1;
+    private int countGrass = 3;
+    private int countRock = 3;
+    private int countTree = 3;
 
     public void nextTurn() {
-
+        moveCounter++;
     }
 
     public void startSimulation() {
         map = new Map(10,10);
-
+        //Creation grass entity
+        for (int i = 1; i <= countGrass; i++) {
+            Grass grass = new Grass(generateCoordinates());
+            Set<Grass> grasses = map.getGrasses();
+            grasses.add(grass);
+            map.setGrasses(grasses);
+        }
+        //Creation rock entity
+        for (int i = 1; i <= countRock; i++) {
+            Rock rock = new Rock(generateCoordinates());
+            Set<Rock> rocks = map.getRocks();
+            rocks.add(rock);
+            map.setRocks(rocks);
+        }
+        //Creation tree entity
+        for (int i = 1; i <= countTree; i++) {
+            Tree tree = new Tree(generateCoordinates());
+            Set<Tree> trees = map.getTrees();
+            trees.add(tree);
+            map.setTrees(trees);
+        }
+        //Creation herbivore entity
+        for (int i = 1; i <= countHerbivore; i++) {
+            Herbivore herbivore = new Herbivore(2,2, generateCoordinates());
+            Set<Herbivore> herbivores = map.getHerbivores();
+            herbivores.add(herbivore);
+            map.setHerbivores(herbivores);
+        }
+        //Creation predator entity
+        for (int i = 1; i <= countPredator; i++) {
+            Predator predator = new Predator(3,2,1, generateCoordinates());
+            Set<Predator> predators = map.getPredators();
+            predators.add(predator);
+            map.setPredators(predators);
+        }
     }
 
     public void pauseSimulation() {
