4f3a71d19f7290282157cf90b41222f5089a960d
Mikhail Bakulin <mibak@yandex.ru>
2023-11-13 15:35:21 +0400

    Add methods for finding of the nearest Entity: findNearestEntity, findGrass, findHerbivore, findPathLength.

--- src/main/java/org/example/Map.java
+++ src/main/java/org/example/Map.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import org.example.creatures.Creature;
+import org.example.creatures.Herbivore;
+import org.example.creatures.Predator;
+import org.example.statics.Grass;
+
 import java.util.HashMap;
 
 public class Map {
@@ -36,6 +41,50 @@ public class Map {
 
     public void setHeight(int height) {
         this.height = height;
+    }
+
+    public Coordinates findNearestEntity(Creature creature) {
+        if (creature instanceof Herbivore) {
+            return findGrass((Herbivore) creature);
+        } else {
+            return findHerbivore((Predator) creature);
+        }
+    }
+
+    private Coordinates findGrass(Herbivore herbivore) {
+        Coordinates grassCoordinates = new Coordinates(0,0);
+        int minPathLength = findPathLength(new Coordinates(1,1),new Coordinates(width,height));
+        for (Coordinates coordinates: map.keySet()) {
+            if (map.get(coordinates) instanceof Grass) {
+                int pathLength = findPathLength(herbivore.getCoordinates(),coordinates);
+                if (pathLength < minPathLength) {
+                    minPathLength = pathLength;
+                    grassCoordinates = coordinates;
+                }
+            }
+        }
+        return grassCoordinates;
+    }
+
+    private Coordinates findHerbivore(Predator predator) {
+        Coordinates herbivoreCoordinates = new Coordinates(0,0);
+        int minPathLength = findPathLength(new Coordinates(1,1),new Coordinates(width,height));
+        for (Coordinates coordinates: map.keySet()) {
+            if (map.get(coordinates) instanceof Herbivore) {
+                int pathLength = findPathLength(predator.getCoordinates(),coordinates);
+                if (pathLength < minPathLength) {
+                    minPathLength = pathLength;
+                    herbivoreCoordinates = coordinates;
+                }
+            }
+        }
+        return herbivoreCoordinates;
+    }
+
+    private int findPathLength(Coordinates coordinates1, Coordinates coordinates2) {
+        int deltaX = Math.abs(coordinates1.getX() - coordinates2.getX());
+        int deltaY = Math.abs(coordinates1.getY() - coordinates2.getY());
+        return (int) Math.sqrt(deltaY * deltaY + deltaX * deltaX);
     }
 
     @Override
