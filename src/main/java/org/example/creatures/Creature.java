82e1798880b0f51c3eec8404577c7e17bb96770f
Mikhail Bakulin <mibak@yandex.ru>
2023-11-22 20:47:41 +0300

    Refactor Creature, Herbivore and Predator classes.

--- src/main/java/org/example/creatures/Creature.java
+++ src/main/java/org/example/creatures/Creature.java
@@ -4,7 +4,9 @@ import org.example.Coordinates;
 import org.example.Entity;
 import org.example.SimulationMap;
 
-public abstract class Creature extends Entity {
+import java.util.HashMap;
+
+public class Creature extends Entity {
     protected final int velocity;
     protected int life;
     protected Coordinates coordinates;
@@ -37,5 +39,35 @@ public abstract class Creature extends Entity {
 
 
 
-    public abstract Coordinates makeMove(SimulationMap simulationMap);
+    public Coordinates makeMove(SimulationMap simulationMap) {
+        HashMap<Coordinates, Entity> currentMap = new HashMap<>(simulationMap.getMap());
+        Coordinates coordinatesAfterMoving = new Coordinates(this.getCoordinates().getX(),this.getCoordinates().getY());
+        for (int i = 1; i <= this.velocity; i++) {
+            Coordinates entityCoordinate = simulationMap.findNearestEntity(this);
+            int y = this.coordinates.getY();
+            int x = this.coordinates.getX();
+            Coordinates moveByY = new Coordinates(x,y);
+            Coordinates moveByX = new Coordinates(x,y);
+            if (entityCoordinate.getY() > y) {
+                moveByY.setY(y + 1);
+            } else if (entityCoordinate.getY() < y) {
+                moveByY.setY(y - 1);
+            }
+            if (entityCoordinate.getX() > x) {
+                moveByX.setX(x + 1);
+            } else if (entityCoordinate.getX() < x) {
+                moveByX.setX(x - 1);
+            }
+            if (simulationMap.findPathLength(moveByX, entityCoordinate) >= simulationMap.findPathLength(moveByY, entityCoordinate)) {
+                if (!currentMap.containsKey(moveByY) || moveByY.equals(entityCoordinate)) {
+                    coordinatesAfterMoving = moveByY;
+                }
+            } else {
+                if (!currentMap.containsKey(moveByX) || moveByX.equals(entityCoordinate)) {
+                    coordinatesAfterMoving = moveByX;
+                }
+            }
+        }
+        return coordinatesAfterMoving;
+    }
 }
--- src/main/java/org/example/creatures/Herbivore.java
+++ src/main/java/org/example/creatures/Herbivore.java
@@ -23,44 +23,4 @@ public class Herbivore extends Creature {
                 " y=" + coordinates.getY() +
                 '}';
     }
-
-    @Override
-    public Coordinates makeMove(SimulationMap simulationMap) {
-        HashMap<Coordinates, Entity> currentMap = new HashMap<>(simulationMap.getMap());
-        Coordinates coordinatesAfterMoving = new Coordinates(this.getCoordinates().getX(),this.getCoordinates().getY());
-        for (int i = 1; i <= this.velocity; i++) {
-            Coordinates grassCoordinates = simulationMap.findNearestEntity(this);
-            int y = this.coordinates.getY();
-            int x = this.coordinates.getX();
-            Coordinates moveByY = new Coordinates(x,y);
-            Coordinates moveByX = new Coordinates(x,y);
-            if (grassCoordinates.getY() > y) {
-                moveByY.setY(y + 1);
-            } else if (grassCoordinates.getY() < y) {
-                moveByY.setY(y - 1);
-            }
-            if (grassCoordinates.getX() > x) {
-                moveByX.setX(x + 1);
-            } else if (grassCoordinates.getX() < x) {
-                moveByX.setX(x - 1);
-            }
-            if (simulationMap.findPathLength(moveByX, grassCoordinates) >= simulationMap.findPathLength(moveByY, grassCoordinates)) {
-                if (!currentMap.containsKey(moveByY) || moveByY.equals(grassCoordinates)) {
-                    coordinatesAfterMoving = moveByY;
-                }
-            } else {
-                if (!currentMap.containsKey(moveByX) || moveByX.equals(grassCoordinates)) {
-                    coordinatesAfterMoving = moveByX;
-                }
-            }
-            /*if (this.coordinates.equals(grassCoordinates) & this.life < maxLife) {
-                this.life++;
-            }
-            currentMap.remove(new Coordinates(x, y), this);
-            currentMap.put(this.coordinates, this);
-            map.setMap(currentMap);
-             */
-        }
-        return coordinatesAfterMoving;
-    }
 }
