60c26d1ee17d652d92c72df441166a2af55b5642
Mikhail Bakulin <mibak@yandex.ru>
2023-11-29 22:56:44 +0300

    Realize findPath method for BFS. Not finished.

--- src/main/java/org/example/SimulationMap.java
+++ src/main/java/org/example/SimulationMap.java
@@ -5,6 +5,8 @@ import org.example.creatures.Herbivore;
 import org.example.creatures.Predator;
 import org.example.statics.Grass;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SimulationMap {
@@ -122,6 +124,49 @@ public class SimulationMap {
         Coordinates coordinates = entity.getCoordinates();
         if (entity != null && !map.containsKey(coordinates)) {
             map.put(coordinates,entity);
+        }
+    }
+
+    public void findPath(Coordinates coordinates) {
+        ArrayDeque<Coordinates> queue = new ArrayDeque<>();
+        ArrayList<Coordinates> visitedCell = new ArrayList<>();
+        visitedCell.add(coordinates);
+        queue.add(new Coordinates(coordinates.getX() - 1, coordinates.getY()));
+        queue.add(new Coordinates(coordinates.getX(), coordinates.getY() + 1));
+        queue.add(new Coordinates(coordinates.getX() + 1, coordinates.getY()));
+        queue.add(new Coordinates(coordinates.getX(), coordinates.getY() - 1));
+        while (!queue.isEmpty()) {
+            Coordinates visitCell = queue.poll();
+            visitedCell.add(visitCell);
+            if (map.containsKey(visitCell)) {
+                if (map.get(coordinates) instanceof Herbivore) {
+                    if (map.get(visitCell) instanceof Grass) {
+                        break;
+                    }
+                }
+                if (map.get(coordinates) instanceof Predator) {
+                    if (map.get(visitCell) instanceof Herbivore) {
+                        break;
+                    }
+                }
+            } else {
+                Coordinates cellA = new Coordinates(visitCell.getX() - 1, visitCell.getY());
+                if (!visitedCell.contains(cellA) && cellA.getX() >= 0) {
+                    queue.add(cellA);
+                }
+                Coordinates cellB = new Coordinates(visitCell.getX(), visitCell.getY() + 1);
+                if (!visitedCell.contains(cellB) && cellA.getY() <= height) {
+                    queue.add(cellB);
+                }
+                Coordinates cellC = new Coordinates(visitCell.getX() + 1, visitCell.getY());
+                if (!visitedCell.contains(cellC) && cellA.getX() <= width) {
+                    queue.add(cellC);
+                }
+                Coordinates cellD = new Coordinates(visitCell.getX(), visitCell.getY() - 1);
+                if (!visitedCell.contains(cellD) && cellA.getY() >= 0) {
+                    queue.add(cellD);
+                }
+            }
         }
     }
 
